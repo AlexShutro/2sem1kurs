078cf28d521c39337cd5b5789f0ed1f3621bfd99
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2024-10-22 17:26:22 +0300

    Add files via upload

---
+++ laba9/src/sortTable.data.js
@@ -0,0 +1,65 @@
+export const tableList = [
+	{
+		name: 'Travis Scott - Astroworld',
+		price: '160',
+		stock: '3',
+		image: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/e0/Travis_Scott_Astroworld.jpg/411px-Travis_Scott_Astroworld.jpg',
+		description: 'Astroworld — третий студийный альбом американского рэпера и продюсера Трэвиса Скотта, вышедший 3 августа 2018 года. В записи альбома приняли участие многочисленные приглашённые певцы, включая таких как Кид Кади, Фрэнк Оушен, Дрейк, Стиви Уандер, The Weeknd, Джеймс Блейк, 21 Savage, Quavo, Juice WRLD, Don Toliver и многие другие. Диск получил положительные отзывы музыкальных критиков и сразу дебютировал на позиции № 1 в американском хит-параде Billboard 200.',
+		newProduct: false,
+		sale: '9'
+	},
+	{
+		name: 'A$AP Rocky - Testing',
+		price: '170',
+		stock: '96',
+		image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/Testing_%28%D0%B0%D0%BB%D1%8C%D0%B1%D0%BE%D0%BC%29.jpg',
+		description: 'Testing — третий студийный альбом американского рэпера ASAP Rocky. Он был выпущен 25 мая 2018 года на лейблах ASAP Worldwide, Polo Grounds Music и RCA Records. В создании альбома принимали участие приглашенные исполнители, такие как: Фрэнк Оушен, Кид Кади, Скепта, French Montana, Kodak Black, Дев Хайнс и FKA twigs. Альбом был спродюсирован Эктором Дельгадо и самим ASAP Rocky.',
+		newProduct: true,
+		sale: '0'
+	},
+	{
+		name: 'XXXTentacion - ?',
+		price: '200',
+		stock: '10',
+		image: 'https://upload.wikimedia.org/wikipedia/ru/thumb/3/31/XXXTentacion.jpg/411px-XXXTentacion.jpg',
+		description: '? — второй студийный альбом американского рэпера XXXTentacion и последний, выпущенный при его жизни. Он был выпущен 16 марта 2018 года на лейблах Bad Vibes Forever, Caroline Distribution и Capitol Music Group. Альбом был в основном спродюсирован частым соавтором Джоном Каннингемом и включает в себя совместные работы с PnB Rock, Matt OX, Трэвисом Баркером, Joey Badass и других.',
+		newProduct: false,
+		sale: '22'
+	},
+	{
+		name: '2Pac - All Eyez On Me',
+		price: '300',
+		stock: '37',
+		image: 'https://upload.wikimedia.org/wikipedia/ru/4/49/%211996_-_All_Eyez_on_Me.jpg',
+		description: 'All Eyez on Me — двойной альбом Тупака Шакура, был издан 13 февраля 1996 г. на лейбле Death Row Records. Этот альбом очень сильно повлиял на рэп-музыку в целом и определил развитие жанра на годы вперёд. Уже к июню 1998-го года пластинка достигла 9-кратной платины, а к июлю 2014-го альбом перешагнул порог десятимиллионого тиража, удостоившись тем самым бриллиантового статуса RIAA и став одним из самых продаваемых хип-хоп альбомов за всё время существования жанра. Критики и фанаты рэп-музыки сходятся в одном: это один из лучших альбомов жанра всех времён и народов',
+		newProduct: false,
+		sale: '12'
+	},
+	{
+		name: 'The Weeknd - After Hours',
+		price: '170',
+		stock: '0',
+		image: 'https://upload.wikimedia.org/wikipedia/ru/c/c1/The_Weeknd_-_After_Hours.png',
+		description: 'After Hours — четвёртый студийный альбом канадского певца The Weeknd, выпущенный 20 марта 2020 года. Название было представлено 13 февраля 2020 года. В период с ноября по август было выпущено четыре сингла: «Heartless», «Blinding Lights», «In Your Eyes» и «Save Your Tears».',
+		newProduct: false,
+		sale: '0'
+	},
+	{
+		name: 'Justin Bieber - Justice',
+		price: '135',
+		stock: '0',
+		image: 'https://upload.wikimedia.org/wikipedia/ru/thumb/5/57/Justice_by_Justin_Bieber.jpg/411px-Justice_by_Justin_Bieber.jpg',
+		description: 'Justice — шестой студийный альбом канадского певца Джастина Бибера, вышедший 19 марта 2021 года на лейблах Def Jam Recordings, RBMG, SB Projects, Schoolboy и Universal Music. Бибер стал первым сольным артистом-мужчиной, который одновременно дебютировал на вершине Billboard Hot 100 и Billboard 200 ',
+		newProduct: false,
+		sale: '30'
+	},
+	{
+		name: 'Kendrick Lamar - DAMN.',
+		price: '190',
+		stock: '7',
+		image: 'https://upload.wikimedia.org/wikipedia/ru/thumb/a/a7/Kendrick_Lamar_Damn_cover.jpg/411px-Kendrick_Lamar_Damn_cover.jpg',
+		description: 'DAMN. — четвёртый студийный альбом американского хип-хоп исполнителя Кендрика Ламара. Альбом вышел 14 апреля 2017 года под руководством лейблов Aftermath Entertainment, Interscope Records и Top Dawg Entertainment. Альбому предшествовал сингл «Humble», который дебютировал на второй строчке в хит-параде Billboard 100.',
+		newProduct: true,
+		sale: '10'
+	},
+]
