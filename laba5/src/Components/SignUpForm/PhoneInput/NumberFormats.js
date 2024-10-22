589f1ad3ac8d28293cfd5be13074ae43255f74cc
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2024-10-22 17:23:17 +0300

    Add files via upload

---
+++ laba5/src/Components/SignUpForm/PhoneInput/NumberFormats.js
@@ -0,0 +1,64 @@
+export const NumberFormats =
+    [
+        {
+            id: 1,
+            name: "Belarus",
+            code: '+375',
+            mask: 'хххх (хх) ххх-хх-хх',
+            operators: [
+                'МТС', 'А1', 'life:)'
+            ],
+            count: 13,
+
+        }, {
+        id: 2,
+        name: "Russia",
+        code: '+7',
+        mask: 'хх (ххх) ххх-хх-хх',
+        operators: [
+            'МТС', 'Билайн', 'Мегафон', 'Tele2'
+        ],
+        count: 12,
+
+    }, {
+        id: 3,
+        name: "Ukraine",
+        code: '+380',
+        mask: 'хххх (хх) ххх-хх-хх',
+        operators: [
+            'Lifecell', 'Vodafone', 'Київстар'
+        ],
+        count: 13,
+
+    }, {
+        id: 4,
+        name: "Poland",
+        code: '+48',
+        mask: 'ххх ххх-ххх-ххх',
+        operators: [
+            'Orange', 'Play', 'Plus', 'T-mobile'
+        ],
+        count: 12,
+
+    }, {
+        id: 5,
+        name: "Lithuania",
+        code: '+370',
+        mask: 'хххх (хх) ххх-хх-хх',
+        operators: [
+            'Telia', 'Bite', 'Tele2'
+        ],
+        count: 13,
+
+    }, {
+        id: 6,
+        name: "Latvia",
+        code: '+371',
+        mask: 'хххх хххх-хххх',
+        operators: [
+            'LMT', 'Tele2', 'Bite'
+        ],
+        count: 12,
+
+    },
+    ]
