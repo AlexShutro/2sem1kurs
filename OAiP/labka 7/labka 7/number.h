a553b15b94b292d36694dd069140a82de0da096c
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2024-10-27 17:40:41 +0300

    Add files via upload

---
+++ OAiP/labka 7/labka 7/number.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include<iostream>
+#include<tchar.h>
+#include<fstream>
+#include <string>
+
+struct Queue
+{
+	int info;
+	Queue* next;
+};
+void create(Queue** begin, Queue** end, int p);
+void view(Queue* begin);
+Queue* maxElem(Queue* begin);
+void size(Queue* begin);
+void delAll(Queue** begin);
+void del3(Queue** begin);
